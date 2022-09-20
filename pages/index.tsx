39215f2575ca6897029ab654980c1f6cf85c2fc6
roadd <ryssbin@gmail.com>
2022-09-20 12:40:25 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,10 +18,9 @@ const IndexPage = () => (
         <h3 className="font-light text-xl uppercase tracking-wider">
           systemd / coming soon
         </h3>
-        <h1 className="text-7xl lg:text-9xl font-extrabold">We’r blowing up</h1>
+        <h1 className="text-7xl lg:text-9xl font-extrabold">Wen? Soon!</h1>
         <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto">
-          We`re under construction. Check back for an update soon. Stay in
-          touch!
+          We`re under heavy construction.
         </p>
       </div>
       <form className="relative z-10 mx-10 lg:max-w-xl lg:mx-auto">
